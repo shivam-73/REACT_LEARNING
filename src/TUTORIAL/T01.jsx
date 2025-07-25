0488ef889b5ddcd618db79785767acf916dbf157
shivam <maddheshiyashivam786@gmail.com>
2025-07-25 18:59:13 +0530

    What is Component

---
+++ src/TUTORIAL/T01.jsx
@@ -0,0 +1,21 @@
+
+/*
+// import T1 from "./TUTORIAL/T1";
+
+function App(){
+  return (
+    <>
+    <Fruit/>
+    <h1>An apple a day keeps the doctor away</h1>
+    </>
+  )
+}
+
+function Fruit(){
+  return (
+    <h1>Apple</h1>
+  )
+}
+export default App; 
+
+*/
