a932502a31ed8cc20a0752922e07cba9988a4758
shivam <maddheshiyashivam786@gmail.com>
2025-07-25 18:40:12 +0530

    Import Export Component

---
+++ src/TUTORIAL/T2.jsx
@@ -0,0 +1,27 @@
+function Login(){  //Default Export 
+    return(
+        <div>
+            <h1>User Login</h1>
+        </div>
+    )
+}
+
+export function Profile(){ //Named export
+    return(
+        <div>
+            <h1>Profile</h1>
+        </div>
+    )
+}
+
+export function Settings(){
+    return (
+        <div>
+            <h1>Settings</h1>
+        </div>
+    )
+}
+
+export default Login;
+
+export const userKey = "agfsjgiau14q";
