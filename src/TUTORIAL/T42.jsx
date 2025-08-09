1eb36685bfc4176bd51f786af4d184c13a898966
shivam <maddheshiyashivam786@gmail.com>
2025-08-09 16:17:36 +0530

    Fragment in React js

---
+++ src/TUTORIAL/T42.jsx
@@ -0,0 +1,22 @@
+import { Fragment } from "react";
+function App() {
+  return (
+    <>
+      <Data/>
+      <Data/>
+      <Data/>
+      <Data/>
+      <Data/>
+      <Data/>
+    </>
+  )
+}
+
+export default App;
+function Data(){
+  return (
+    <>
+    <span>Hello</span><b>User</b>
+    </>
+  )
+}
