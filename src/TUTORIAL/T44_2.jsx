df97cb487587505044ad4fb7a041e713b9f641f2
shivam <maddheshiyashivam786@gmail.com>
2025-08-10 18:02:50 +0530

    Context API

---
+++ src/TUTORIAL/T44_2.jsx
@@ -0,0 +1,9 @@
+import ClassComponent from "./T44_3"
+export default function College(){
+    return(
+         <div style={{backgroundColor:"orange",padding:'10px'}}>
+            <h1>College Component</h1>
+            <ClassComponent/>
+        </div>
+    )
+}
