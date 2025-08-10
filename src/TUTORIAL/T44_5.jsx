4e2a71034d70c0cf63cb53ca2a4431f8ec6d76b0
shivam <maddheshiyashivam786@gmail.com>
2025-08-10 18:03:59 +0530

    Context API

---
+++ src/TUTORIAL/T44_5.jsx
@@ -0,0 +1,11 @@
+import { useContext } from "react"
+import { SubjectContext } from "./T44_1.jsx"
+
+export default function Subject(){
+    const subject = useContext(SubjectContext)
+    return(
+        <div style={{backgroundColor:"red",padding:'10px'}}>
+            <h1>Subject is : {subject}</h1>
+        </div>
+    )
+}
