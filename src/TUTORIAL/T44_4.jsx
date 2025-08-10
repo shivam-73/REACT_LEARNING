36d880637f035a1432228a073d99d4470ae51b02
shivam <maddheshiyashivam786@gmail.com>
2025-08-10 18:03:17 +0530

    Context API

---
+++ src/TUTORIAL/T44_4.jsx
@@ -0,0 +1,10 @@
+import Subject from "./T44_5";
+
+export default function  Student(){
+    return(
+        <div style={{backgroundColor:"green",padding:'10px'}}>
+            <h1>Student Component</h1>
+            <Subject/>
+        </div>
+    )
+} 
