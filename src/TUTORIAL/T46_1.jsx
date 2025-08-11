864bf816b4580f8a1baec5d97672c1db0d80788b
shivam <maddheshiyashivam786@gmail.com>
2025-08-11 17:03:20 +0530

    Basic React-Router page

---
+++ src/TUTORIAL/T46_1.jsx
@@ -0,0 +1,24 @@
+export default function About(){
+    return <h1>About Page</h1>
+}
+
+// import { Routes , Route,Link } from "react-router";
+// import About from "./TUTORIAL/T46_1";
+// import Login from "./TUTORIAL/T46_2";
+// function App() {
+
+//   return (
+//     <>
+//     <Link to ='/'>Home</Link>
+//     <Link to='/about'>About</Link>
+//     <Link to='/login'>Login</Link>
+//     <Routes>
+//       <Route path="/" element={<h1>Home Page</h1>}/>
+//       <Route path="/about" element={<About/>}/>
+//       <Route path="/login" element={<Login/>}/>
+//     </Routes>
+//     </>
+//   )
+// }
+
+// export default App;
